2cf5ffd5d370b8c4de104e557bd42308ec2c0e37
Tth <mtoutouhi@gmail.com>
2019-04-17 21:50:17 +0200

    reglesjeu

---
+++ src/reglesJeu.php
@@ -0,0 +1,58 @@
+<?php
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../public/assets/css/style.css" />
+    <title>Wild Easter Egg Game</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="box box-bg sb12 shadow-lg">Bonjour {{nom du joueur}}, voici les regles du jeu ! </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-flex">
+    <div class="container py-3 d-flex align-content-center flex-wrap">
+        <div class="card shadow-lg p-3 mb-5 rounded-circle">
+            <div class="row ">
+                <div class="col-md-4">
+                    <img src="http://www.franceregion.fr/assets/frontend/media/images/Lapin2.png" class="w-100">
+                </div>
+                <div class="textbox col-md-6 px-3">
+                    <div class="card-block px-3">
+                        <h4 class="card-title">Lorem ipsum dolor sit amet</h4>
+                        <p class="card-text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                        <p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
